bfd04cafcaeb5f7f8921535200a0763bed785b4c
barronbrn <qwebaron7@gmail.com>
2025-02-11 13:45:17 +0700

    Halaman Penyewaan dan Confirm Penyewaan

--- app/Models/Penyewaan.php
+++ app/Models/Penyewaan.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
+use App\Models\Peralatan;
 use Illuminate\Database\Eloquent\Model;
 
 class Penyewaan extends Model
 {
     //
-    
+    protected $fillable = [
+        'user_id', // Tambahkan ini
+        'peralatan_id',
+        'durasi',
+        'total_harga',
+        'metode_pembayaran',
+        'status',
+    ];
+
+    public function peralatan()
+    {
+        return $this->belongsTo(Peralatan::class, 'peralatan_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -115,7 +115,7 @@
 
             {{-- Peralatan Dengan Kategori Alat Masak --}}
             <div class="mt-16">
-                <h3 class="text-gray-600 text-2xl font-medium">Kategori Camping</h3>
+                <h3 class="text-gray-600 text-2xl font-medium">Kategori Alat Masak</h3>
                 <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
                     @foreach ($peralatan as $item)
                         @if ($item->jenis == 'Alat Masak')
